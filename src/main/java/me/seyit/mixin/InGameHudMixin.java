25b856687965e347d99b5871d4db5933c6c9b50b
Seyit <69308754+IseyitThe@users.noreply.github.com>
2026-06-18 21:49:03 +0300

    26.2

--- src/main/java/me/seyit/mixin/InGameHudMixin.java
+++ src/main/java/me/seyit/mixin/InGameHudMixin.java
@@ -6,8 +6,8 @@ import net.minecraft.client.DeltaTracker;
 import net.minecraft.client.KeyMapping;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Font;
-import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.GuiGraphicsExtractor;
+import net.minecraft.client.gui.Hud;
 import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -16,7 +16,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(Gui.class)
+@Mixin(Hud.class)
 public class InGameHudMixin {
     
     @Shadow @Final private Minecraft minecraft;
